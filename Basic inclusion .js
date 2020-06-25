3cfea34800bc5c36530578b6c0af0a2000453cb5
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-06-25 21:04:57 +0530

    Create Basic inclusion .js

---
+++ Basic inclusion .js
@@ -0,0 +1,12 @@
+<!DOCTYPE Html>
+<html>
+  <head> </head>
+  <body>
+   <script type="text/javascript"   src="/path/to/react.js" ></script>
+   <script type="text/javascript"   src="/path/to/react.js" ></script>
+   <script type="text/javascript" >
+   //Use react javasscript code here or in a separate file 
+   </script>
+   </body>
+   </html>
+   
