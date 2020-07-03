a3790684d9b365a3e1ceda7de694784914442632
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-07-03 21:55:48 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+//import Radium, {Stylesheet} from 'radium';
+import classes from './App.css';
+import Persons from '../components/Persons /Persons';
+//import ErrorBoundary from './Error boundary/ErrorBoundary ';
+//contain component like App.js that contain state and its handling should not have too much of jsx
+class App extends Component {
+    //This is a state full component 
+    state= {
+      
