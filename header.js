ce00b1a1912558a82a2e4fd30107e62627dac083
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-07-04 16:10:22 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+//Component 
+
+import Header from './components/header'
+const App = () =>  {
+   return (
+      <div> 
+      Header 
+      </div>
+      )
+      
+      }
+ReactDOM.render(</App>,document.querySelector('#root'));
+
