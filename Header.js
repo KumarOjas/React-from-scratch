225b9db8896f70ee9c10f50f6f218dc04ecdd255
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-07-04 16:38:57 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const user = {
+  name:'Francis',
+  lastName:'Jones',
+  age:16
+  }
+ const Header = ()  => {
+      return  (
+      <div>
+         <div>{user.name} </div>
+          <div>{user.lastname} </div>
+           <div>{user.age} </div>
+           </div>
+           )
+     }
+    
+    export default Header;
+    
+           
