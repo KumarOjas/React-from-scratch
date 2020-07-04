372559c75a3150e0c8bd8ceb853fe2828ae53cf8
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-07-04 15:17:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-const App = () => {
-    return <h1>Hello World !!! </h1>
-    }
-    ReactDOM.render(<App/>,document.querySelector(#root));
-    
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+ReactDom.render(<App/>.document.getElemementById('root'));
+registerServiceWorker();
