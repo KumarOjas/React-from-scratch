5f642ac7b3faaef7b05dc2b2f18ecbee7a12d107
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-07-05 12:38:14 +0530

    Create header1.js

---
+++ header1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Header = () => {
+  return (
+    <header>
+    <div>Logo </div>
+    <input type="text"/>
+    </header>
+    )
+    }
+export default Header;
+    
