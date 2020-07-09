3974d2c95ce64662d0cee41f36e58d59353c9c49
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-07-10 00:41:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //Components
-
-import Header from "./components/header" 
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(<App/>,document.getElementBYId('root'));
+registerServiceWorker();
 const App =() => {
   return (
       <div class="newElement">
@@ -29,3 +32,90 @@ const Header = () => {
     return <div> This is my new Header </div>
 }
 export default Header ;
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(</App>,document.getElementById('root '));
+ registerServiceWorker();
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+    return <h1> Hello world !!!  </h1>
+  
+}
+  ReactDOM.render (</App>,document.querhySelector('#root'));
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+                   
+                   
+const App = ()  => {
+      return {
+          <div className="newElement">
+        <h1> Hello React !! </h1>
+        <div> Hey </div>
+        </div>
+        )}
+    
+    
+  ReactDOM.render(</App>,document.querySelector ('#root'));
+                  
+                   
+   import React  from 'react';
+   import ReactDOM from 'react-dom';
+                  
+    const App = () => {
+       return  (
+          <div className="new Element ">
+           <h1> Hello React !! </h1>
+           <div> Hey  </div>
+         </div>
+         )}
+  
+  
+  ReacvtDOm.render(</App>,document.querySelector('#root));
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
